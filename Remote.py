c83d111d48e5cbee100b369e1ed40501169a255a
ShreevathsaGP <gpshreevathsa@gmail.com>
2020-06-20 17:49:34 +0530

    twt files

--- Remote.py
+++ Remote.py
@@ -5,4 +5,4 @@ from Camera import Camera
 import time, datetime, os
 import numpy as np
 
-Camera().start('server', host='192.168.0.105') # default port = 4999
+Camera().start('server', host='192.168.0.105', port=5000) # default port = 4999
