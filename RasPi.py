ce8847baa8d8f919713fd564b6d9627f0e278106
ShreevathsaGP <gpshreevathsa@gmail.com>
2020-06-20 20:22:27 +0530

    testing and training

--- RasPi.py
+++
@@ -1,8 +0,0 @@
-# RasPi.py (Raspberry-Pi File)
-
-# importing modules
-from Camera import Camera
-import time, datetime, os
-import numpy as np
-
-Camera().start('client', host='192.168.0.105') # default port = 4999
---
+++ training/RasPi.py
@@ -0,0 +1,8 @@
+# RasPi.py (Raspberry-Pi File)
+
+# importing modules
+from Camera import Camera
+import time, datetime, os
+import numpy as np
+
+Camera().start('client', host='192.168.0.105') # default port = 4999
